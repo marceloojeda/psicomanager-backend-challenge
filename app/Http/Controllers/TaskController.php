c41663ca21b82e8690bed935fffd5a7988676db4
leonarrdo <leozimbrq@gmail.com>
2025-01-29 20:47:57 -0300

    refactor: adicionando cache para consultas e adicionando tratativas para erros

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\Task;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Cache;
 
 class TaskController extends Controller
 {
@@ -19,25 +21,29 @@ class TaskController extends Controller
     function index(Request $request)
     {
         if (!empty($request->input('user_id'))) {
-
-            $results = Task::where(['user_id' => $request->input('user_id')])->get();
+            $tasks = Cache::remember('tasks_user_' . $request->input('user_id'), 60, function () use ($request) {
+                return Task::when(!empty($request->input('user_id')), function ($query) use ($request) {
+                    return $query->where('user_id', $request->input('user_id'));
+                })->select('id', 'user_id', 'description', 'status')->get();
+            });
         } else {
-
-            $results = Task::all();
+            $tasks = Cache::remember('tasks_all', 60, function () {
+                return Task::select('id', 'user_id', 'description', 'status')->get();
+            });
         }
 
-        $retorno = [];
-        foreach ($results as $task) {
-
-            $retorno[] = $task->toArray();
-        }
-
-        return response()->json($retorno);
+        return response()->json($tasks);
     }
 
     function get($taskId)
     {
-        $task = Task::where('id', $taskId)->firstOrFail();
-        return response()->json($task);
+        try {
+            $task = Task::select('id', 'user_id', 'description', 'status')->where('id', $taskId)->firstOrFail();
+            return response()->json($task, Response::HTTP_OK);
+        } catch (\Illuminate\Database\Eloquent\ModelNotFoundException $e) {
+            return response()->json(['message' => 'Task não encontrada'], Response::HTTP_NOT_FOUND);
+        } catch (\Throwable $th) {
+            return response()->json(['message' => 'Erro ao buscar task', 'error' => $th->getMessage()], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
     }
 }
